44a6bacf9798cab56afd1e758393b75a0745dfb5
ThomsonTang <guiketang@gmail.com>
2017-10-17 14:03:23 +0800

    DEV: LRU算法的实现

---
+++ problem-practice/src/main/java/com/thomson/algs/lru/LruLinkedHashMap.java
@@ -0,0 +1,47 @@
+package com.thomson.algs.lru;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 类说明
+ *
+ * @author Thomson Tang
+ */
+public class LruLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private final Lock lock = new ReentrantLock();
+    private final int maxCapacity;
+
+    public LruLinkedHashMap(int maxCapacity) {
+        super(maxCapacity, DEFAULT_LOAD_FACTOR, true);
+        this.maxCapacity = maxCapacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > maxCapacity;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        try {
+            lock.lock();
+            return super.containsKey(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        try {
+            lock.lock();
+            return super.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
